cfcf16c3f46cf129552d726f5367adc14bf4a5c2
UlianaL13 <uliana912006@gmail.com>
2025-04-18 12:12:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import asyncio #ассинхронный ввод-вывод
+from aiogram import Bot, Dispatcher
+from utils.handlers import router
+
+token = '7908037638:AAHYIKclCOY4UhISLiojkkk1Tdl2GxtLlNs'
+
+async def main():
+    bot = Bot(token)
+    dp=Dispatcher()
+    dp.include_router (router) #подключаем из headlers
+    await dp.start_polling(bot) #ждем стартовых команд от бота
+
+if __name__=='__main__':
+    asyncio.run(main())
