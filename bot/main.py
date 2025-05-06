64fb0824fbd30e90acb07a40a3ae966819e5aae3
UlianaL13 <uliana912006@gmail.com>
2025-05-06 11:47:01 +0300

    Add files via upload

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,16 @@
 import asyncio #ассинхронный ввод-вывод
 from aiogram import Bot, Dispatcher
 from utils.handlers import router
+from service.src.model.basemodel import BaseRecognitionModel
+
 
 token = '7908037638:AAHYIKclCOY4UhISLiojkkk1Tdl2GxtLlNs'
 
 async def main():
     bot = Bot(token)
-    dp=Dispatcher()
+    dp = Dispatcher()
     dp.include_router (router) #подключаем из headlers
+
     await dp.start_polling(bot) #ждем стартовых команд от бота
 
 if __name__=='__main__':
