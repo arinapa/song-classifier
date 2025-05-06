1cdc637bd84ad1e36d4bee6dfa7a1b196d1b140f
UlianaL13 <uliana912006@gmail.com>
2025-05-06 11:47:27 +0300

    Add files via upload

--- bot/utils/handlers.py
+++ bot/utils/handlers.py
@@ -1,10 +1,17 @@
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
 from aiogram.filters import CommandStart, Command
 from aiogram import Router, F
+import logging
 
 import utils.keyboards as kb
+import utils.voices as v
 
 router = Router() #какой функцией обработать команду
+
+@router.errors_handler
+async def errors_handler(update, exception):
+    logging.error(f'Update: {update} caused error: {exception}')
+    return True
 
 #обработка команд через /
 @router.message(CommandStart()) #/start
@@ -20,6 +27,12 @@ async def cmd_info (message: Message):
 async def cmd_info (message: Message):
     await message.answer ('Телеграм-бот для идентификации трека, играющего в окружении пользователя.')
 
+@router.message(F.content_type == [ContentType.VOICE])
+async def voice_message_handler(message: Message):
+    voice = await message.voice.get_file()
+    path = "/files/voices"
+
+    await v.handle_file(file=voice, file_name=f"{voice.file_id}.ogg", path=path)
 
 #@router.message() #декоратор
 #async def cmd_start (message: Message):
