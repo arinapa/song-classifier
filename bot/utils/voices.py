71b8dc2cdd7397c02815a95741d307a118b5d145
UlianaL13 <uliana912006@gmail.com>
2025-05-07 14:33:37 +0300

    Delete bot/utils/voices.py

--- bot/utils/voices.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from aiogram.types import File
-from service.src.model.basemodel import BaseRecognitionModel
-
-async def handle_file(bot, file: File, file_name: str, path: str):
-    #Path(f"{path}").mkdir(parents=True, exist_ok=True) #тут должен быть путь
-    await bot.download_file(file_path=file.file_path, destination=f"{path}/{file_name}")
-    #BaseRecognitionModel
