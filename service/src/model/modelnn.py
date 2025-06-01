fdfcbf1b6db70c52413252c31e76b423c444aa0a
arinapa <parasinaa@gmail.com>
2025-06-01 09:55:42 +0300

    добавлен datadealer в modelnn, немного изменена логика получения embeddings

--- service/src/model/modelnn.py
+++ service/src/model/modelnn.py
@@ -6,11 +6,11 @@ from sklearn.neighbors import NearestNeighbors
 from transformers import ClapProcessor, ClapModel
 import joblib
 
-from service.src.model.song import Song
-from service.src.model.basemodel import BaseRecognitionModel  
+from model.song import Song
+from model.basemodel import BaseRecognitionModel  
 
 class CLAP_KNN_Model(BaseRecognitionModel):
-    def __init__(self, music_library_path, n_neighbors=5):
+    def __init__(self, music_library_path, datadealer, n_neighbors=5):
 
 
         super().__init__(music_library_path)
@@ -24,29 +24,30 @@ class CLAP_KNN_Model(BaseRecognitionModel):
         self.song_paths = []
         self.knn = None
 
+        self.datadealer = datadealer
+        self.build_index(self.music_library_path, datadealer)
 
-        self.build_index(self.music_library_path)
 
-
-    def get_audio_embedding(self, file_path):
-        waveform, sample_rate = torchaudio.load(file_path)
+    def get_audio_embeddings(self, waveform, sample_rate):
         inputs = self.processor(audios=waveform, sampling_rate=sample_rate, return_tensors="pt")
         with torch.no_grad():
             embedding = self.model.get_audio_features(**inputs)
         return embedding[0].numpy() 
 
-    def build_index(self, folder_path):
+    def get_audio_embedding(self, music_file):
+        waveform, sample_rate = torchaudio.load(music_file)
+        return self.get_audio_embeddings(waveform, sample_rate)
+
+    def build_index(self, folder_path, datadealer):
         # собираем эмбеддинги для всех аудиофайлов
         self.embeddings = []
         self.song_paths = []
 
-        for filename in os.listdir(folder_path):
-            if filename.endswith(".wav"):
-                full_path = os.path.join(folder_path, filename)
-                
-                emb = self.get_audio_embedding(full_path)
-                self.embeddings.append(emb)
-                self.song_paths.append(full_path)
+        for index in datadealer:
+            song_data, waveform, sample_rate = datadealer(index)
+            emb = self.get_audio_embeddings(waveform, sample_rate)
+            self.embeddings.append(emb)
+            self.song_paths.append(song_data)
 
         self.embeddings = np.vstack(self.embeddings)
         self.knn = NearestNeighbors(n_neighbors=self.n_neighbors, metric="cosine")
@@ -60,7 +61,7 @@ class CLAP_KNN_Model(BaseRecognitionModel):
             query_emb = self.get_audio_embedding(music_file).reshape(1, -1)
             distances, indices = self.knn.kneighbors(query_emb, n_neighbors=1)
             best_match_path = self.song_paths[indices[0][0]]
-            return Song(path=best_match_path) 
+            return Song(path=best_match_path['Название файла']) 
         except Exception as e:
             print(f"Error during song recognition: {e}")
             return None
