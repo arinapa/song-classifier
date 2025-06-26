ad60020597032fed885ed7aef59432ba3cd41594
arinapa <parasinaa@gmail.com>
2025-06-26 15:41:26 +0300

    Update and rename song.py to song.py

--- service/src/data/song.py
+++ service/src/data/song.py
@@ -1,16 +1,9 @@
 from dataclasses import dataclass
 from typing import Optional
 
-
-# потом подредактируем
-
-
 @dataclass
 class Song:
-    file_path: str
-    similarity: float
-    start_time: float #для себя, чтобы понять что к чему
-    end_time: float
+    path: str
     name: Optional[str] = None
     artist: Optional[str] = None
     year: Optional[int] = None
