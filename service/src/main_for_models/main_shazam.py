8bf521b8564afa6b4d56d5200cba3af0122a89cf
arinapa <parasinaa@gmail.com>
2025-06-26 17:05:06 +0300

    Update main_shazam.py

--- service/src/main_for_models/main_shazam.py
+++ service/src/main_for_models/main_shazam.py
@@ -1,11 +1,11 @@
 import os
-from model.shazam_model_last import ShazamModel
+from model.shazam_model import ShazamModel
 from data.s3_DataDealer import S3DataDealer  
 
 datadealer = S3DataDealer("songs/dataset_songs.csv")
 
-model = ShazamModel(None, load_from="../models/model_shazam.pkl" ) 
-# model = ShazamModel(None, datadealer, 2048, 512, 3, 5.0, (15, 30), 5, 1, 3, None) # для обучения и сохранения
+model = ShazamModel(None, None, 2048, 256, 2, 5.0, (15, 30), 5, 1, 5, load_from="../models/model_shazam.pkl" )
+# model = ShazamModel(None, datadealer, 2048, 256, 2, 5.0, (15, 30), 5, 1, 5, None) # для обучения и сохранения
 # model.save("../models/model_shazam.pkl")
 
 
