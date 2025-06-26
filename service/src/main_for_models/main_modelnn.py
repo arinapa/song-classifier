532cd64cb10957891fd0c07287aabaa1faaf6de0
arinapa <parasinaa@gmail.com>
2025-06-26 15:39:14 +0300

    Update and rename main_modelnn.py to main_modelnn.py

--- service/src/main_for_models/main_modelnn.py
+++ service/src/main_for_models/main_modelnn.py
@@ -2,14 +2,12 @@ import os
 from model.modelnn import CLAP_KNN_Model
 from data.s3_DataDealer import S3DataDealer  
 
-music_library_path = "path/to/your/music/library"
 
-
-model = CLAP_KNN_Model(None, None, n_neighbors=5, load_from="model_data.pkl" )
+model = CLAP_KNN_Model(None, None, n_neighbors=5, load_from="../models/model_nn.pkl" )
 
 
 
-query_file = "songs/nerv.mp3"
+query_file = "../songs/nerv.mp3"
 result = model(query_file)
 print(f"Распознанная песня: {result.path if result else 'Не распознано'}")
 
